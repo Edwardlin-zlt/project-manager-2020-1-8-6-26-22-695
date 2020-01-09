6293bbf164c39b895e1ca05c01b89a61dbe8173f
Edwardlin-zlt <edwardlin.zlt@gmail.com>
2020-01-09 16:48:54 +0800

    [Lintao] {feat}-{StoryNo.02}: ProjectInfoList Done!

---
+++ js/ajax.js
@@ -0,0 +1,28 @@
+window.ajax = function(options) {
+  options = {
+    url: options.url || "",
+    method: options.method || "get",
+    headers: options.headers || {},
+    data: options.data || null,
+    onSuccess: options.success || function(result) {},
+    onFail: options.fail || function(result) {}
+  };
+
+  
+  var xhr = new XMLHttpRequest();
+  xhr.open(options.method, options.url, true);
+  for (key in options.headers){
+    xhr.setRequestHeader(key, options.headers[key]);
+  }
+  xhr.onreadystatechange = function() {
+    // TODO 201 succeed
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200 || xhr.status === 201) {
+        options.onSuccess(xhr.responseText);
+      } else {
+        options.onFail(xhr.status);
+      }
+    }
+  };
+  xhr.send(options.data);
+};
--- js/index.js
+++ js/index.js
@@ -1,16 +1,12 @@
-var allMissionNum = document.querySelector(".all .count-num")
-var undoneMissionNum = document.querySelector(".undone .count-num")
-var undergoingMissionNum = document.querySelector(".undergoing .count-num")
-var doneMissionNum = document.querySelector(".done .count-num")
-var undonePercentage = document.querySelector(".undone .count-percentage")
-var undergoingPercentage = document.querySelector(".undergoing .count-percentage")
-var donePercentage = document.querySelector(".done .count-percentage")
+const API_ROOT = "http://localhost:3000/projects"
+var infoList = document.getElementById("info-list")
 
 function updateNumberOrPercentage(targetEle, numberOrPercentage){
   targetEle.innerHTML = numberOrPercentage;
 }
 
 function updateAllMissionCard(number){
+  var allMissionNum = document.querySelector(".all .count-num")
   allMissionNum.innerHTML = number;
 }
 
@@ -23,3 +19,45 @@ function updateMissionCard(missionType, number){
   updateNumberOrPercentage(missionNum, number);
   updateNumberOrPercentage(missionPercentage, percentage);
 }
+
+function getListData() {
+  options = {
+    url: API_ROOT,
+    method: "get",
+    success: function(result){
+        console.log(result);
+        let data = JSON.parse(result)
+        renderInfoList(data);
+    }
+  }
+  ajax(options);
+}
+
+function renderInfoList(data) {
+  if (!Array.isArray(data) && !data instanceof Array) {
+    return false;
+  }
+
+  var listHeadStr = `
+    <li class="info-list-head">
+      <span class="project-name">项目名称</span>
+      <span class="project-describe">项目描述</span>
+      <span class="deadline">截止日期</span>
+      <span class="status">状态</span>
+      <span class="operation">操作</span>
+    </li>`
+
+  infoList.innerHTML = data.reduce((acc, cur) => {
+    var curStr = `
+      <li class="info-row" data-id="${cur.id}">
+        <span class="project-name">${cur.name}</span>
+        <span class="project-describe">${cur.description}</span>
+        <span class="deadline">${cur.endTime}</span>
+        <span class="status status-${cur.status.toLowerCase()}">${cur.status}</span>
+        <span class="operation"><button class="del-btn">删除</button></span>
+      </li> `
+    return (acc += curStr);
+  }, listHeadStr)
+}
+
+getListData();
