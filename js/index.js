9aead9263f2bb4a7048bc143317f90e28f722cd0
Edwardlin-zlt <edwardlin.zlt@gmail.com>
2020-01-10 20:16:39 +0800

    [Lintao {feat}-{StoryNo.03}: Done!

--- js/index.js
+++ js/index.js
@@ -1,21 +1,54 @@
-const API_ROOT = "http://localhost:3000/projects"
-var infoList = document.getElementById("info-list")
+const API_ROOT = "http://localhost:3000/projects";
+var infoList = document.getElementById("info-list");
+var alertWindow = document.querySelector(".alert-window")
+var alertWindowMask = document.querySelector(".mask")
+var toDelId = -1;
 
-function updateNumberOrPercentage(targetEle, numberOrPercentage){
+function main() {
+  getListData();
+
+  infoList.addEventListener("click", function(e) {
+    switch (true) {
+      case (e.target.className.includes("del-btn")):
+        toDelId = e.target.parentElement.parentElement.getAttribute("data-id");
+        showConfirmWindow();
+        break;
+    }
+  });
+
+  alertWindow.addEventListener("click", function(e){
+    switch (true) {
+      case e.target.className.includes("close-window")
+        || e.target.className.includes("cancel"):
+        alertWindow.style.visibility = "hidden";
+        alertWindowMask.style.visibility = "hidden"
+        toDelId = -1;
+        break;
+      case e.target.className.includes("sure"):
+        alertWindow.style.visibility = "hidden";
+        alertWindowMask.style.visibility = "hidden"
+        delInfoRowData(toDelId);
+    }
+  })
+}
+
+function updateNumberOrPercentage(targetEle, numberOrPercentage) {
   targetEle.innerHTML = numberOrPercentage;
 }
 
-function updateAllMissionCard(number){
-  var allMissionNum = document.querySelector(".all .count-num")
+function updateAllMissionCard(number) {
+  var allMissionNum = document.querySelector(".all .count-num");
   allMissionNum.innerHTML = number;
 }
 
-function updateMissionCard(missionType, number){
+function updateMissionCard(missionType, number) {
   var allMissionNum = document.querySelector(".all .count-num");
   var percentage = number / parseInt(allMissionNum.innerHTML);
   percentage = Math.round(percentage * 100) + "%";
-  var missionNum = document.querySelector(`.${missionType} .count-num`)
-  var missionPercentage = document.querySelector(`.${missionType} .count-percentage`)
+  var missionNum = document.querySelector(`.${missionType} .count-num`);
+  var missionPercentage = document.querySelector(
+    `.${missionType} .count-percentage`
+  );
   updateNumberOrPercentage(missionNum, number);
   updateNumberOrPercentage(missionPercentage, percentage);
 }
@@ -24,12 +57,11 @@ function getListData() {
   options = {
     url: API_ROOT,
     method: "get",
-    success: function(result){
-        console.log(result);
-        let data = JSON.parse(result)
-        renderInfoList(data);
+    success: function(result) {
+      let data = JSON.parse(result);
+      renderInfoList(data);
     }
-  }
+  };
   ajax(options);
 }
 
@@ -45,7 +77,7 @@ function renderInfoList(data) {
       <span class="deadline">截止日期</span>
       <span class="status">状态</span>
       <span class="operation">操作</span>
-    </li>`
+    </li>`;
 
   infoList.innerHTML = data.reduce((acc, cur) => {
     var curStr = `
@@ -53,11 +85,35 @@ function renderInfoList(data) {
         <span class="project-name">${cur.name}</span>
         <span class="project-describe">${cur.description}</span>
         <span class="deadline">${cur.endTime}</span>
-        <span class="status status-${cur.status.toLowerCase()}">${cur.status}</span>
+        <span class="status status-${cur.status.toLowerCase()}">${
+      cur.status
+    }</span>
         <span class="operation"><button class="del-btn">删除</button></span>
-      </li> `
+      </li> `;
     return (acc += curStr);
-  }, listHeadStr)
+  }, listHeadStr);
 }
 
-getListData();
+function showConfirmWindow() {
+  alertWindow.style.visibility = "visible";
+  alertWindowMask.style.visibility = "visible"
+}
+
+function delInfoRowData(id) {
+  options = {
+    url: API_ROOT + `/${id}`,
+    method: "DELETE",
+    success: function(result) {
+      console.log(`Deleted Item ID: ${id}`)
+      delInfoRowEle(id);
+    }
+  };
+  ajax(options);
+}
+
+function delInfoRowEle(id) {
+  let toDelEle = infoList.querySelector(`[data-id = "${id}"]`);
+  infoList.removeChild(toDelEle);
+}
+
+main();
